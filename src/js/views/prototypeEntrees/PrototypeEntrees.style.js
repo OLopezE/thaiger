10046bccb4d0012469822d325d2e38038f310eb6
Oscar francisco Lopez escobar <olopeze@outlook.com>
2023-06-25 22:48:53 -0600

    changing entrees

--- src/js/views/prototypeEntrees/PrototypeEntrees.style.js
+++ src/js/views/prototypeEntrees/PrototypeEntrees.style.js
@@ -1,8 +1,12 @@
 import styled from "styled-components";
 
 const EntreesWrapper = styled.section`
+  max-height: 100vh;
   max-height: 100svh;
   overflow: scroll;
+  padding: 10rem 2rem;
+  display: grid;
+  gap: 2rem;
 `;
 
 export { EntreesWrapper };
