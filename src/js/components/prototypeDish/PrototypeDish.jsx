ae7eea6f9f0d802bde18f7a4d2ccf280d1d1115a
Oscar francisco Lopez escobar <olopeze@outlook.com>
2023-06-27 18:52:49 -0600

    adding icons

--- src/js/components/prototypeDish/PrototypeDish.jsx
+++ src/js/components/prototypeDish/PrototypeDish.jsx
@@ -1,9 +1,11 @@
-import { PrototypeDishWrapper } from "./PrototypeDish.style"
+import { PrototypeDishWrapper } from "./PrototypeDish.style";
+import { FaShrimp, FaEgg, FaPepperHot } from 'react-icons/fa6';
+import { GiPeanut } from "react-icons/gi";
 
 const PrototypeDish = ({ dish }) => {
   return (
     <PrototypeDishWrapper>
-      <h3>{dish.name}</h3>
+      <h3>{dish.name} <FaShrimp /> <FaEgg /> <FaPepperHot /> <GiPeanut /></h3>
       <p>{dish.description}</p>
       <p>{dish.price}</p>
     </PrototypeDishWrapper>
--- src/js/components/prototypeDish/PrototypeDish.style.js
+++ src/js/components/prototypeDish/PrototypeDish.style.js
@@ -2,7 +2,6 @@ import styled from "styled-components";
 
 const PrototypeDishWrapper = styled.div`
   padding-right: 2rem;
-
   text-align: center;
 `;
 
